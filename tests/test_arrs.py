66622e2c2f0a7c832d11755cea9555f1b099afea
Anna <kokshiro@gmail.com>
2023-05-20 23:09:45 +1000

    new tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -8,17 +8,70 @@ class TestArrs(unittest.TestCase):
         arr = [1, 2, 3, 4]
         self.assertEqual(get(arr, 1, "test"), 2)
 
-    def test_get_zero_index(self):
+    def test_get_negative_index(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(get(arr, -1, "test"), "test")
+
+    def test_get_empty_index(self):
         arr = []
         self.assertEqual(get(arr, 0, "test"), "test")
 
-    def test_my_slice_positive_index(self):
+    def test_get_zero_index(self):
+        arr = [1, 2, 3]
+        self.assertEqual(get(arr, 0, "test"), 1)
+
+    def test_my_slice_positive_all_index(self):
         arr = [1, 2, 3, 4]
         self.assertEqual(my_slice(arr, 1, 3), [2, 3])
+
+    def test_my_slice_positive_start_index(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(my_slice(arr, 1), [2, 3, 4])
+
+    def test_my_slice_out_of_range_start_index(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(my_slice(arr, 10), [])
+
+    def test_my_slice_out_of_range_end_index(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(my_slice(arr, 1, 29), [2, 3, 4])
+    def test_my_slice_negative_start_index(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(my_slice(arr, -1), [4])
+
+    def test_my_slice_negative_end_index(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(my_slice(arr, 1, -3), [])
+
+    def test_my_slice_negative_all_index(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(my_slice(arr, -3, -1), [2, 3])
+
+    def test_my_slice_negative_all_index_out_of_range(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(my_slice(arr, -5, -1), [1, 2, 3])
+
+    def test_my_slice_negative_end_index_out_of_range(self):
+        arr = [1, 2, 3, 4]
+        self.assertEqual(my_slice(arr, 1, -6), [])
 
     def test_my_slice_zero_index(self):
         arr = [1, 2, 3]
         self.assertEqual(my_slice(arr, 1), [2, 3])
+
+    def test_my_slice_empty_index(self):
+        arr = []
+        self.assertEqual(my_slice(arr, 1), [])
+
+    def test_my_slice_empty_empty_index(self):
+        arr = [1, 2]
+        self.assertEqual(my_slice(arr), [1, 2])
+
+    def test_my_slice_empty(self):
+        arr = []
+        self.assertNotEqual(my_slice(arr), [1])
+
+
 
 
 if __name__ == '__main__':
